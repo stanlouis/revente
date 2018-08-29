d4d5f8d1bd77e4ceff2783375b43f0f79eb8f4ce
Stanley Louis <stanlouis@msn.com>
2018-08-29 14:20:28 -0400

    Add EventListItem component

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import EventDashboard from '../../features/event/EventDashboard/EventDashboard';
 import NavBar from '../../features/nav/NavBar/NavBar';
 import { Container } from 'semantic-ui-react';
+
 class App extends Component {
   render() {
     return (
