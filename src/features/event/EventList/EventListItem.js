b947cb5fcc0d738c8da697e9f685c80172d9dc55
Stanley Louis <stanlouis@msn.com>
2018-09-05 16:08:20 -0400

    Add new event via EventForm.js

--- src/features/event/EventList/EventListItem.js
+++ src/features/event/EventList/EventListItem.js
@@ -28,7 +28,8 @@ class EventListItem extends Component {
         </Segment>
         <Segment secondary>
           <List horizontal>
-            {event.attendees.map(attendee => (
+            {/* render event.attendee only if there are any */}
+            {event.attendee && event.attendees.map(attendee => (
               <EventListAttendee key={attendee.id} attendee={attendee}/>
             ))}
           </List>
