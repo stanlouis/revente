05a303da3da464c400e7dc1261a70313e0a29719
Stanley Louis <stanlouis@msn.com>
2018-10-09 09:17:29 -0400

    Use higher order component withRouter to add history to navbar

--- src/features/nav/NavBar/NavBar.js
+++ src/features/nav/NavBar/NavBar.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Menu, Container, Button } from 'semantic-ui-react';
-import { NavLink, Link } from 'react-router-dom';
+import { NavLink, Link, withRouter } from 'react-router-dom';
 import SignedOutMenu from '../Menus/SignedOutMenu';
 import SignedInMenu from '../Menus/SignedInMenu';
 
@@ -19,6 +19,7 @@ class NavBar extends Component {
     this.setState({
       authenticated: false
     });
+    this.props.history.push('/');
   };
   render() {
     const { authenticated } = this.state;
@@ -30,17 +31,22 @@ class NavBar extends Component {
             Re-vents
           </Menu.Item>
           <Menu.Item as={NavLink} to="/events" name="Events" />
-          <Menu.Item as={NavLink} to="/people" name="People" />
-          <Menu.Item>
-            <Button
-              as={Link}
-              to="/createEvent"
-              floated="right"
-              positive
-              inverted
-              content="Create Event"
-            />
-          </Menu.Item>
+          {authenticated && (
+            <Menu.Item as={NavLink} to="/people" name="People" />
+          )}
+          {authenticated && (
+            <Menu.Item>
+              <Button
+                as={Link}
+                to="/createEvent"
+                floated="right"
+                positive
+                inverted
+                content="Create Event"
+              />
+            </Menu.Item>
+          )}
+
           {authenticated ? (
             <SignedInMenu signOut={this.handleSignOut} />
           ) : (
@@ -52,4 +58,4 @@ class NavBar extends Component {
   }
 }
 
-export default NavBar;
+export default withRouter(NavBar);
